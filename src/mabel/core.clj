394bb37b165d17f5d691fd12663e9b513111ed62
niten <niten@fudo.org>
2024-04-21 16:33:32 -0700

    What the FUCK is going on?

--- src/mabel/core.clj
+++ src/mabel/core.clj
@@ -98,13 +98,9 @@
       (let [id (UUID/randomUUID)]
         (mebot/room-image! mebot-room snapshot (str id ".jpg")))
       (swap! context
-             (fn [ctx]
-               (assert (map? ctx) "Context is not a map!")
-               (assert (contains? ctx :recents) "Context is missing :recents key!")
-               (assert (contains? ctx :silence-map) "Context is missing :silence-map key!")
-               (update
-                (update ctx :recents add-snapshot snapshot)
-                :silence-map add-silence camera))))
+             (->* (pthru)
+                  (update :recents add-snapshot snapshot)
+                  (update :silence-map add-silence camera))))
     context))
 
 (defmethod handle-update! :quit
