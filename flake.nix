ac9fcacda12bfbb683adaf7a8622091377a13e0d
niten <niten@fudo.org>
2025-01-26 21:51:19 -0800

    Update deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Mabel Home Bot";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     utils.url = "github:numtide/flake-utils";
     helpers = {
       url = "github:fudoniten/fudo-nix-helpers";
