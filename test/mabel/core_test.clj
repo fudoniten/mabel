f0091e1b659c53ad8af0aca7147c47ef20c3ea26
niten <niten@fudo.org>
2025-06-08 11:54:13 -0700

    feat: Add tests for core functions

--- test/mabel/core_test.clj
+++ test/mabel/core_test.clj
@@ -1,53 +1,50 @@
 (ns mabel.core-test
-  (:require [mabel.core :as sut]
-            [clojure.test :as t]
-            [clj-time.core :as t]))
+  (:require [mabel.core :as core]
+            [clojure.test :refer [deftest is testing run-tests]]
+            [clj-time.core :refer [minutes seconds now plus]]))
 
-(t/deftest test-pthru
-  (t/testing "pthru prints and returns its argument"
-    (let [obj {:foo "bar"}]
-      (t/is (= obj (with-out-str (sut/pthru obj)))))))
+(deftest test-parallelism
+  (testing "parallelism returns number of processors + 1"
+    (is (= (+ 1 (.availableProcessors (Runtime/getRuntime)))
+             (core/parallelism)))))
 
-(t/deftest test-parallelism
-  (t/testing "parallelism returns number of processors + 1"
-    (t/is (= (+ 1 (.availableProcessors (Runtime/getRuntime)))
-             (sut/parallelism)))))
-
-(t/deftest test-snapshot-cache
-  (let [cache (sut/snapshot-cache 2)]
-    (t/testing "has-snapshot? returns false for empty cache"
-      (t/is (not (sut/has-snapshot? cache "foo"))))
+(deftest test-snapshot-cache
+  (let [cache (core/snapshot-cache 2)]
+    (testing "has-snapshot? returns false for empty cache"
+      (is (not (core/has-snapshot? cache "foo"))))
     
-    (t/testing "add-snapshot adds snapshot"
-      (let [cache (sut/add-snapshot cache "foo")]
-        (t/is (sut/has-snapshot? cache "foo"))))
+    (testing "add-snapshot adds snapshot"
+      (let [cache (core/add-snapshot cache "foo")]
+        (is (core/has-snapshot? cache "foo"))))
     
-    (t/testing "add-snapshot removes oldest when full"
-      (let [cache (-> (sut/snapshot-cache 2) 
-                      (sut/add-snapshot "foo")
-                      (sut/add-snapshot "bar") 
-                      (sut/add-snapshot "baz"))]
-        (t/is (not (sut/has-snapshot? cache "foo")))
-        (t/is (sut/has-snapshot? cache "bar"))
-        (t/is (sut/has-snapshot? cache "baz"))))))
+    (testing "add-snapshot removes oldest when full"
+      (let [cache (-> (core/snapshot-cache 2)
+                      (core/add-snapshot "foo")
+                      (core/add-snapshot "bar")
+                      (core/add-snapshot "baz"))]
+        (is (not (core/has-snapshot? cache "foo")))
+        (is (core/has-snapshot? cache "bar"))
+        (is (core/has-snapshot? cache "baz"))))))
 
-(t/deftest test-silence-map
-  (let [sm (sut/silence-map 10)]
-    (t/testing "silenced? is false for non-silenced camera"
-      (t/is (not (sut/silenced? sm "foo"))))
+(deftest test-silence-map
+  (let [sm (core/silence-map 10)]
+    (testing "silenced? is false for non-silenced camera"
+      (is (not (core/silenced? sm "foo"))))
     
-    (t/testing "add-silence silences camera"
-      (let [sm (sut/add-silence sm "foo")]
-        (t/is (sut/silenced? sm "foo"))))
+    (testing "add-silence silences camera"
+      (let [sm (core/add-silence sm "foo")]
+        (is (core/silenced? sm "foo"))))
     
-    (t/testing "silence expires after pause-time"
-      (let [sm (sut/add-silence sm "foo" (t/now))]
-        (t/is (not (sut/silenced? sm "foo")))))))
+    (testing "silence expires after pause-time"
+      (let [sm (core/add-silence sm "foo" (now))]
+        (is (not (core/silenced? sm "foo")))))))
 
-(t/deftest test-parse-time
-  (t/testing "parse-time handles single element"
-    (t/is (= (t/seconds 42) (sut/parse-time ["42s"]))))
+(deftest test-parse-time
+  (testing "parse-time handles single element"
+    (is (= (seconds 42) (core/parse-time ["42s"]))))
 
-  (t/testing "parse-time handles multiple elements"
-    (t/is (= (t/plus (t/minutes 1) (t/seconds 30))
-             (sut/parse-time ["1m" "30s"])))))
+  (testing "parse-time handles multiple elements"
+    (is (= (plus (minutes 1) (seconds 30))
+           (core/parse-time ["1m" "30s"])))))
+
+(run-tests)
